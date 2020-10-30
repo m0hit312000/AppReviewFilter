e7b713843bba3df1a45fb7178745c17a8bf6afd6
Mohit <mm8085574989@gmail.com>
2020-10-30 21:39:45 +0530

    Side panel 1

--- src/components/SideFilterComponent.js
+++ src/components/SideFilterComponent.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { IoIosSearch } from 'react-icons/io';
 
 const SideFilter = () => {
    return(
       <div className="side_filter">
-          Hello 
+          <div className="input_field">
+             <IoIosSearch style={{marginLeft: "1rem", position: "absolute"}} className="search_icon" />
+             <input className="search_filter" type="text" placeholder="Search" />
+          </div>
+          <div className="time_filer">
+             <select className="time_select">
+                <option>
+
+                </option> 
+             </select>  
+          </div>
       </div>
    );
 }
